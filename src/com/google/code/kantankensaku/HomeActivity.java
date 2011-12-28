f5226df1d0ae7aa83ab15a654f63c486d7789ab1
Hiroshi Ichikawa <ichikawa@google.com>
2011-12-28 22:35:43 +0900

    Initial version. Only large screen is supported.

---
+++ src/com/google/code/kantankensaku/HomeActivity.java
@@ -0,0 +1,261 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+// Author: Hiroshi Ichikawa
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.code.kantankensaku;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class HomeActivity extends Activity {
+
+  private enum KanaType {
+    SEION, DAKUON, HANDAKUON, YOUON,
+  }
+  
+  private static final int MIN_DISPLAY_WIDTH = 1024;
+  private static final int MIN_DISPLAY_HEIGHT = 600;
+  
+	private TextView queryField;
+	private KeyboardView keyboardView;
+	private Button searchButton;
+	
+	private Keyboard gojuonKeyboard;
+	private Keyboard simpleAsciiKeyboard;
+  private HashMap<KanaType, HashMap<Character, Character>> charMap;
+  
+  // WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED in API Level 11
+  public static final int FLAG_HARDWARE_ACCELERATED = 0x01000000;
+  
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home_big);
+    queryField = (TextView)findViewById(R.id.queryField);
+    keyboardView = (KeyboardView)findViewById(R.id.keyboardView);
+    searchButton = (Button)findViewById(R.id.searchButton);
+    
+    gojuonKeyboard = new Keyboard(this, R.xml.gojuon_keyboard);
+    simpleAsciiKeyboard = new Keyboard(this, R.xml.simple_ascii_keyboard);
+    keyboardView.setKeyboard(gojuonKeyboard);
+    keyboardView.setOnKeyboardActionListener(onKeyboardAction);
+    searchButton.setOnClickListener(onSearchButtonClick);
+    createCharMap();
+    checkDisplaySize();
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.home_menu, menu);
+    return true;
+  }
+  
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    super.onMenuItemSelected(featureId, item);
+    switch (item.getItemId()) {
+    case R.id.originalHomeItem:
+      Intent intent = new Intent(Intent.ACTION_MAIN);
+      intent.addCategory(Intent.CATEGORY_HOME);
+      startActivity(Intent.createChooser(intent, null));
+      break;
+    case R.id.settingsItem:
+      startActivity(new Intent("android.settings.SETTINGS"));
+      break;
+    default:
+      throw new RuntimeException("unimplemented item");  
+    }
+    return true;
+  }
+  
+  private void checkDisplaySize() {
+    Display display = getWindowManager().getDefaultDisplay();
+    log("display: %d,%d", display.getWidth(), display.getHeight());
+    if (display.getWidth() < MIN_DISPLAY_WIDTH || display.getHeight() < MIN_DISPLAY_HEIGHT) {
+      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+      builder.setTitle(getResources().getString(R.string.app_name));
+      builder.setMessage(
+          "このデバイスでは" + getResources().getString(R.string.app_name) +
+          "を利用できません。\n1024×600以上の解像度が必要です。");
+      builder.setPositiveButton("OK",
+          new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+              finish();
+            }
+          });
+      builder.create().show();
+    }
+  }
+  
+  private void backSpace() {
+    String query = queryField.getText().toString();
+    if (query.length() > 0) {
+      queryField.setTextKeepState(query.substring(0, query.length() - 1));
+    }
+  }
+  
+  private OnClickListener onSearchButtonClick = new OnClickListener() {
+		public void onClick(View arg0) {
+		  search();
+		}
+  };
+  
+  private OnClickListener onHomeButtonClick = new OnClickListener() {
+    public void onClick(View arg0) {
+      queryField.setText("");
+    }
+  };
+  
+  private void search() {
+    String query = queryField.getText().toString();
+    if (query.length() == 0) return;
+    Intent intent = new Intent(HomeActivity.this, BrowserActivity.class);
+    intent.putExtra("q", queryField.getText().toString());
+    // Use startActivityForResult() instead of startAcrivity() to clear the input when
+    // user come back here (i.e. on onActivityResult()).
+    startActivityForResult(intent, 0);
+  }
+  
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    queryField.setText("");
+    keyboardView.setKeyboard(gojuonKeyboard);
+  }
+  
+  
+  private void modifyChar(KanaType type) {
+    String query = queryField.getText().toString();
+    if (query.length() == 0) return;
+    Character converted = charMap.get(type).get(query.charAt(query.length() - 1));
+    if (converted != null) {
+      queryField.setTextKeepState(query.substring(0, query.length() - 1) + converted);
+    }
+  }
+  
+  private void createCharMap() {
+    HashMap<KanaType, String> typeToChars = new HashMap<KanaType, String>();
+    String seionChars =
+      "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+    typeToChars.put(KanaType.SEION, seionChars);
+    typeToChars.put(KanaType.DAKUON,
+        "あいゔえおがぎぐげござじずぜぞだぢづでどなにぬねのばびぶべぼまみむめもやゆよらりるれろわをん");
+    typeToChars.put(KanaType.HANDAKUON,
+        "あいうえおかきくけこさしすせそたちつてとなにぬねのぱぴぷぺぽまみむめもやゆよらりるれろわをん");
+    typeToChars.put(KanaType.YOUON,
+        "ぁぃぅぇぉかきくけこさしすせそたちってとなにぬねのはひふへほまみむめもゃゅょらりるれろゎをん");
+    Set<KanaType> types = typeToChars.keySet();
+    charMap = new HashMap<KanaType, HashMap<Character, Character>>();
+    for (KanaType targetType : types) {
+      if (targetType == KanaType.SEION) continue;
+      HashMap<Character, Character> map = new HashMap<Character, Character>();
+      for (int i = 0; i < seionChars.length(); ++i) {
+        if (typeToChars.get(targetType).charAt(i) == seionChars.charAt(i)) continue;
+        for (KanaType sourceType : types) {
+          char source = typeToChars.get(sourceType).charAt(i);
+          // Toggle.
+          char target = sourceType == targetType ?
+              seionChars.charAt(i) : typeToChars.get(targetType).charAt(i);
+          map.put(source, target);
+        }
+      }
+      charMap.put(targetType, map);
+    }
+  }
+  
+  private OnKeyboardActionListener onKeyboardAction = new OnKeyboardActionListener() {
+    
+    public void swipeUp() {
+    }
+    
+    public void swipeRight() {
+    }
+    
+    public void swipeLeft() {
+    }
+    
+    public void swipeDown() {
+    }
+    
+    public void onText(CharSequence arg0) {
+    }
+    
+    public void onRelease(int arg0) {
+    }
+    
+    public void onPress(int arg0) {
+    }
+    
+    public void onKey(int primaryCode, int[] keyCodes) {
+      log("onKey: %d", primaryCode);
+      switch (primaryCode) {
+      case 0:
+        // Do nothing.
+        break;
+      case -400:
+        modifyChar(KanaType.DAKUON);
+        break;
+      case -401:
+        modifyChar(KanaType.HANDAKUON);
+        break;
+      case -402:
+        modifyChar(KanaType.YOUON);
+        break;
+      case -403:
+        queryField.setText("");  // will be removed
+        break;
+      case -100:
+        backSpace();
+        break;
+      case -101:
+        search();
+        break;
+      case -230:
+        keyboardView.setKeyboard(
+            keyboardView.getKeyboard() == gojuonKeyboard ?
+                simpleAsciiKeyboard : gojuonKeyboard);
+        break;
+      default:
+        queryField.append(new String(new char[] { (char)primaryCode }));
+        break;
+      }
+    }
+  };
+  
+  private void log(String format, Object... args) {
+    Log.i("kantankensaku", String.format(format, (Object[])args));
+  }
+  
+}
---
+++ tools/generate_keyboard_xml.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+
+# Copyright 2011 Google Inc. All Rights Reserved.
+# Author: Hiroshi Ichikawa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+type = sys.argv[1]
+# small:
+#key_height = 62
+#vertical_gap = 8
+#kana_x_metrics = [(7, 56), (10, 56), (10, 56), (36, 67), (13, 67), (14, 67), (13, 67), (13, 67), (13, 67), (14, 67), (13, 67), (13, 67), (12, 67),]
+kana_x_metrics = [(8, 71), (12, 71), (12, 70), (45, 84), (16, 84), (18, 83), (17, 83), (17, 83), (17, 83), (18, 83), (17, 83), (17, 83), (16, 83), ]
+ascii_x_metrics = [(119, 84), (16, 84), (16, 84), (18, 83), (17, 83), (17, 83), (15, 84), ]
+key_height = 80
+vertical_gap = 11
+if type == "gojuon":
+  key_labels = [
+      u"123わらやまはなたさかあ",
+      u"456　り　みひにちしきい",
+      u"789をるゆむふぬつすくう",
+      u" 0 　れ　めへねてせけえ",
+      u"   んろよもほのとそこお",
+  ]
+  x_metrics = kana_x_metrics
+
+else:
+  key_labels = [
+      u"123ABCDEFG",
+      u"456HIJKLMN",
+      u"789OPQRSTU",
+      u" 0 VWXYZ  ",
+      u"          ",
+  ]
+  num_width = 0
+  for i in xrange(0, 3):
+    num_width += kana_x_metrics[i][0] + kana_x_metrics[i][1]
+  x_metrics = (kana_x_metrics[0:3] +
+               [((300 - num_width) + ascii_x_metrics[0][0], ascii_x_metrics[0][1])] +
+               ascii_x_metrics[1:])
+
+print '<Keyboard xmlns:android="http://schemas.android.com/apk/res/android"'
+print '    android:horizontalGap="7px"'
+print '    android:verticalGap="%dpx"' % vertical_gap
+print '    android:keyWidth="67px"'
+print '    android:keyHeight="%dpx"' % key_height
+print '    >'
+
+for y in xrange(len(key_labels)):
+  print '    <Row>'
+  for x in xrange(len(key_labels[y])):
+    ch = key_labels[y][x]
+    if ch in (u" ", u"　"):
+      code = 0
+    else:
+      code = ord(ch)
+    (gap, width) = x_metrics[x]
+    if y == len(key_labels) - 1:
+      if x == 0:
+        # space bar
+        width = x_metrics[0][1] + x_metrics[1][0] + x_metrics[1][1] + x_metrics[2][0] + x_metrics[2][1]
+        code = ord(u" ")
+      elif x in [1, 2]:
+        continue
+    if type == 'gojuon' or x < 3:
+      icon = '@drawable/button_big_kana_%d_%d' % (x, y)
+    elif y < 4:
+      icon = '@drawable/button_big_ascii_%d_%d' % (x - 3, y)
+    else:
+      icon = '@drawable/button_big_ascii_6_3'  # empty
+    print ('        <Key android:codes="%d" android:keyIcon="%s" '
+           'android:keyWidth="%dpx" android:horizontalGap="%dpx"/>'
+           % (code, icon, width, gap))
+  print '    </Row>'
+
+#small:
+#bottom_x_metrics =[(7, 188), (38, 143), (5, 143), (5, 143), (5, 143), (5, 194)]
+bottom_x_metrics = [(8, 236), (47, 179), (6, 179), (6, 179), (6, 179), (6, 243), ]
+if type == "gojuon":
+  codes = [
+      -230,  # ASCII mode
+      ord(u"ー"),
+      -400,  # dakuon
+      -401,  # handakuon
+      -402,  # small char
+      #(-403, 168, 10),  # delete all
+      -100,  # backspace
+  ]
+else:
+  codes = [
+      -230,  # hiragana mode
+      0,
+      0,
+      0,
+      0,
+      -100,  # backspace
+  ]
+ 
+type2 = 'kana' if type == 'gojuon' else 'ascii'
+print '    <Row>'
+for i in xrange(len(codes)):
+  print ('        <Key android:codes="%d" android:keyIcon="@drawable/button_big_%s_bottom_%d" '
+         'android:keyWidth="%dpx" android:horizontalGap="%dpx"/>'
+         % (codes[i], type2, i, bottom_x_metrics[i][1], bottom_x_metrics[i][0]))
+print '    </Row>'
+print '</Keyboard>'
