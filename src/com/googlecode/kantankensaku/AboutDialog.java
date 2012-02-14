61305ac8799353c6625ed670e1919b00ff6c6697
Hiroshi Ichikawa <gimite@gmail.com>
2012-02-14 14:32:24 +0900

    Changing package name because com.google.* cannot be used in Android Market. Adding screen size restriction.

--- src/com/googlecode/kantankensaku/AboutDialog.java
+++ src/com/googlecode/kantankensaku/AboutDialog.java
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.code.kantankensaku;
+package com.googlecode.kantankensaku;
 
 import android.app.Dialog;
 import android.content.Context;
